699e89723360b115c9221b4bf4c579c3bb5ba3bc
liminalme <priyanka89kadam@gmail.com>
2018-03-14 18:27:21 -0700

    commit

--- courtcase.R
+++ courtcase.R
@@ -1,26 +1,43 @@
+#Read the data
 stevens=read.csv("stevens.csv")
+#Structure of stevens data
 str(stevens)
+#install and load caTools package
 library("caTools")
+#
 set.seed(3000)
+#Create the split
 split=sample.split(stevens$Reverse, SplitRatio = 0.7)
+#Split the data into training and test set
 train= subset(stevens, split==TRUE)
 train
+#Split the data into training and test set
 test= subset(stevens, split==FALSE)
+#Install and load rpart package for CART
 install.packages("rpart")
 library(rpart)
+#Install and load rpart plot for plotting CART
 install.packages("rpart.plot")
 library(rpart.plot)
+# Create the CART model using rpart function
+# Class tells rpart to build a classification tree since we have a binary outcome
+#minbucket size limits the tree so it does not overfit to our training set
 stevensTree= rpart(Reverse ~ Circuit+Issue+Petitioner+Respondent+LowerCourt+Unconst, data=train,method = "class",control = rpart.control(minbucket = 25))
+#Plot the tree using a prp function
 prp(stevensTree)
+#Predicting cases in the test set using the CART model
+#class will take each test set value and classify into 0 or 1
 predictCART=predict(stevensTree, test,type="class")
+#confusion matrix
 table(test$Reverse, predictCART)
+#accuracy of the model
 accuracy= (41+71)/(41+36+22+71)
 accuracy
-
+#accuracy of baseline model
 table(stevens$Reverse)
 baseline=309/(257+309)
 baseline
-
+#Reciever operator characteristics
 library(ROCR)
 predictROC=predict(stevensTree,test)
 predictROC
@@ -30,14 +47,34 @@ plot(perf)
 
 
 -----------------------------------------------------------
-#Random Forests
+#Random Forests- Designed to improve the prediction accuracy of CART
   
+# install and load random forest package
 install.packages("randomForest")
 library(randomForest)
+#Build the random forest model
+#node size is same as min bucket
+#ntree is no of trees to build
 StevensForest= randomForest(Reverse~Circuit+Issue+Petitioner+Respondent+LowerCourt+Unconst, data=train,nodesize=25, ntree=200)
+# we receive a warning in the previous line of code since we did not specify the method=class
+#Since random forest does not have method, we need to factor the outcome variable
 train$Reverse=as.factor(train$Reverse)
 test$Reverse=as.factor(test$Reverse)
+#Build the model again
 StevensForest= randomForest(Reverse~Circuit+Issue+Petitioner+Respondent+LowerCourt+Unconst, data=train,nodesize=25, ntree=200)
+#confusion matrix
 table(test$Reverse,PredictForest)
+#accuracy of the model
 accuracy=(40+74)/(40+37+19+74)
 accuracy
+
+
+#RESULTS
+#For the 68 cases in October 2002:
+#Overall case predictions:
+#--> Model accuracy: 75%
+#--> Experts accuracy: 59%
+
+# Individual justice predictions:
+#--> Model Accuracy: 67%
+#--> Experts Accuracy: 68%
--- movieLens.R
+++ movieLens.R
@@ -1,27 +1,36 @@
+#Read movieLens data
 movies= read.table("movieLens.txt", header =FALSE, sep="|",quote = "\"")
+#Structure of movieLens dataset
 str(movies)
-View(movies)
+#Add col names to the data set
 colnames(movies)= c("ID","Title","ReleaseDate", "VideoReleaseDate","IMDB","Unknown",
                     "Action", "Adventure","Animation","Childrens","Comedy", "Crime", "Documentary", "Drama",
                     "Fantasy", "FilmNoir","Horror", "Musical", "Mystery", "Romance", "SciFi",
                     "Thriller", "War", "Western")
-str(movies)
+# We wont be using the ID, Releasedate, VideoRelease, IMDB variables, so we will
+#remove them
 movies$ID=NULL
 movies$ReleaseDate= NULL
 movies$VideoReleaseDate= NULL
 movies$IMDB= NULL
 movies= unique(movies)
 str(movies)
-
+ 
+# Compute distances between all data points
 distances= dist(movies[2:20], method = "euclidean")
+#cluster the data points
 clusterMovies=hclust(distances, method="ward.D2")
+#plot the cluster dendogram
 plot(clusterMovies)
+#let us pick 10 clusters and assign the data points to the particular clusters
 clusterGroups= cutree(clusterMovies, k=10)
-
-tapply(movies$Action,clusterGroups, mean)
-tapply(movies$Romance, clusterGroups,mean)
-
+clusterGroups
+#Lets figure out what cluster Men in Black movie in in
 subset(movies, Title=="Men in Black (1997)")
+# Men in Black is in cluster 2
 clusterGroups[257]
+#Look at what other movies are in cluster2
 cluster2= subset(movies, clusterGroups==2)
+cluster2
+#Look at titles of movies in cluster 2
 cluster2$Title[1:10]
